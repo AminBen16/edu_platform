09563cb60ad0030e44595da4f8cef3553f0edc8c
AminBen16 <ainamanibenjamin@gmail.com>
2026-01-11 11:07:37 +0300

    fix: Add missing ApiService import to mobile login screen

--- apps/mobile/lib/screens/login_screen.dart
+++ apps/mobile/lib/screens/login_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
+import '../services/api.dart';
 
 class LoginScreen extends ConsumerStatefulWidget {
   const LoginScreen({super.key});
